1eab0012c81bcea3e64f0e84d99d3de189b60784
leverest96 <myjade0416@gmail.com>
2023-09-03 22:19:10 +0900

    daily commit

--- again/토마토_7569.java
+++ again/토마토_7569.java
@@ -6,84 +6,108 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class 토마토_7569 {
+    static int M, N, H, cnt;
+    static int[] rd = {-1, 1, 0, 0, 0, 0};
+    static int[] cd = {0, 0, -1, 1, 0, 0};
+    static int[] hd = {0, 0, 0, 0, -1, 1};
+    static int[][][] arr;
+    static Queue<Node> queue;
+
+    static class Node {
+        int r;
+        int c;
+        int h;
+
+        Node(int r, int c, int h) {
+            this.r = r;
+            this.c = c;
+            this.h = h;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int M = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-        int H = Integer.parseInt(st.nextToken());
-        int[][][] tomatos = new int[H][N][M];
-        int[] dx = {1, -1, 0, 0, 0, 0};
-        int[] dy = {0, 0, 1, -1, 0, 0};
-        int[] dz = {0, 0, 0, 0, 1, -1};
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
 
-        Queue<Location> queue = new LinkedList<>();
+        arr = new int[N][M][H];
 
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < N; j++) {
+        queue = new LinkedList<>();
+
+        for (int h = 0; h < H; h++) {
+            for (int r = 0; r < N; r++) {
                 st = new StringTokenizer(br.readLine());
-                for (int k = 0; k < M; k++) {
-                    tomatos[i][j][k] = Integer.parseInt(st.nextToken());
-                    if (tomatos[i][j][k] == 1) {
-                        queue.offer(new Location(i, j, k));
+
+                for (int c = 0; c < M; c++) {
+                    arr[r][c][h] = Integer.parseInt(st.nextToken());
+
+                    if (arr[r][c][h] == 1) {
+                        queue.offer(new Node(r, c, h));
                     }
                 }
             }
         }
 
-        while(!queue.isEmpty()){
-            Location t = queue.poll();
+        cnt = 0;
 
-            for(int i = 0; i < 6; i++){
-                int nX = t.x + dx[i];
-                int nY = t.y + dy[i];
-                int nZ = t.z + dz[i];
+        if (check()) bw.write("0");
+        else {
+            cnt = 0;
 
-                if (nX >= 0 && nX < N && nY >= 0 && nY < M && nZ >= 0 && nZ < H && tomatos[nZ][nX][nY] == 0) {
-                    queue.offer(new Location(nZ, nX, nY));
-                    tomatos[nZ][nX][nY] = tomatos[t.z][t.x][t.y] + 1;
-                }
+            bfs();
+
+            if (check()) {
+                bw.write(cnt - 1 + "");
+            } else {
+                bw.write("-1");
             }
-        }
-
-        int result = 0;
-
-        Loop:
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < N; j++) {
-                for (int k = 0; k < M; k++) {
-                    if (tomatos[i][j][k] == 0) {
-                        result = -1;
-                        break Loop;
-                    }
-
-                    result = Math.max(result, tomatos[i][j][k]);
-                }
-            }
-        }
-
-        if (result == 1) {
-            bw.write(0 + "");
-        } else if (result == -1) {
-            bw.write(-1 + "");
-        } else {
-            bw.write(result - 1 + "");
         }
 
         bw.flush();
         bw.close();
     }
 
-    static class Location {
-        int z, x, y;
+    static void bfs() {
+        while (!queue.isEmpty()) {
+            Node now = queue.poll();
 
-        public Location(int z, int x, int y) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
+            for (int i = 0; i < 6; i++) {
+                int nextR = now.r + rd[i];
+                int nextC = now.c + cd[i];
+                int nextH = now.h + hd[i];
+
+                if (nextR >= 0 && nextR < N && nextC >= 0 && nextC < M && nextH >= 0 && nextH < H) {
+                    if (arr[nextR][nextC][nextH] == 0) {
+                        arr[nextR][nextC][nextH] = arr[now.r][now.c][now.h] + 1;
+                        queue.offer(new Node(nextR, nextC, nextH));
+                    }
+                }
+            }
         }
+    }
+
+    static boolean check() {
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < M; k++) {
+                    if (arr[j][k][i] == 0) return false;
+                }
+            }
+        }
+
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < M; k++) {
+                    cnt = Math.max(cnt, arr[j][k][i]);
+                }
+            }
+        }
+
+        return true;
     }
 }
